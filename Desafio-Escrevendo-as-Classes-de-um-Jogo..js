6260a4320748dec2a1c5befdad4b6d58d396271c
raphael-quintana <rapha.quintana13@gmail.com>
2026-04-03 15:53:37 -0300

    Writting Classes of a Game - DIO Challenge

---
+++ Desafio-Escrevendo-as-Classes-de-um-Jogo..js
@@ -0,0 +1,33 @@
+class Hero {
+    constructor(nome, idade, tipo, ataque) {
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+        this.ataque = ataque
+
+        
+        if (this.tipo == "mago"){
+            this.ataque = "magia"
+        } else if (this.tipo == "guerreiro"){
+            this.ataque = "espada"
+        } else if (this.tipo == "monge") {
+            this.ataque = "artes marciais"
+        } else if (this.tipo == "ninja") {
+            this.ataque = "shuriken"
+        }
+       
+    }
+
+    atacar(){
+        console.log(`O ${this.tipo} chamado ${this.nome} atacou usando ${this.ataque}.`)
+    }
+
+}
+
+let doomSlayer = new Hero("Hellwalker", 23, "mago")
+let hideoKojima = new Hero("Solid Snake", 55, "ninja")
+
+
+hideoKojima.atacar()
+doomSlayer.atacar()
+
